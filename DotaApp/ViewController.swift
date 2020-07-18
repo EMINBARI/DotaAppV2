cba2a9abd5db763d033a7b379bf6751e038ed885
Emin <38878208+EMINBARI@users.noreply.github.com>
2020-07-18 19:09:04 +0300

    Initial Commit

---
+++ DotaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DotaApp
+//
+//  Created by Emin Bari on 18.07.2020.
+//  Copyright © 2020 Emin Bari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
